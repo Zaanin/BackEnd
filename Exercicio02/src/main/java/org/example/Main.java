3247ecc2b1c682d70352e94bbe5a4fe74582e136
Zaanin <mateus.195922@alunosatc.edu.br>
2024-03-21 20:05:37 -0300

    exercicio2

---
+++ Exercicio02/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        JogadorGerador jogadorGerador = new JogadorGerador();
+        Jogador jogador = jogadorGerador.geraJogador();
+        Jogador jogador1 = jogadorGerador.geraJogador();
+        Jogador jogador2 = jogadorGerador.geraJogador();
+        Jogador jogador3 = jogadorGerador.geraJogador();
+
+
+        System.out.println(jogador.getDescricao());
+        System.out.println("------");
+        System.out.println(jogador1.getDescricao());
+        System.out.println("------");
+        System.out.println(jogador2.getDescricao());
+        System.out.println("------");
+        System.out.println(jogador3.getDescricao());
+
+    }
+}
